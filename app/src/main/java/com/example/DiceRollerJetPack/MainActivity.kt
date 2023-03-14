5949a9a5f7f0931b612e9129322a4aea29e85a13
Demis Lavrentidis <demis.lavrentidis@openbet.com>
2023-03-14 19:23:00 +0000

    added logic to button for changing images

--- app/src/main/java/com/example/DiceRollerJetPack/MainActivity.kt
+++ app/src/main/java/com/example/DiceRollerJetPack/MainActivity.kt
@@ -4,12 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Button
 import androidx.compose.foundation.Image
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -35,13 +33,22 @@ class MainActivity : ComponentActivity() {
         modifier
             .fillMaxSize()
             .wrapContentSize(Alignment.Center)
+        var result by remember { mutableStateOf(1) }
+        val imageResource = when (result) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
         Column(
             modifier = modifier,
             horizontalAlignment = Alignment.CenterHorizontally)
         {
-            Image(painter = painterResource(id = R.drawable.dice_1), contentDescription = "1")
+            Image(painter = painterResource(id = imageResource), contentDescription = result.toString())
             Spacer(modifier = Modifier.height(16.dp))
-            Button(onClick = { /*TODO*/ }) {
+            Button(onClick = { result = (1..6).random() }) {
                 Text(text = stringResource(id = R.string.roll))
             }
         }
